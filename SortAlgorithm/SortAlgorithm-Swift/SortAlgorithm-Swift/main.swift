0c76f2f67521e0d2b5217500b2adc6dbad9a74a2
Chris Hu <icetime17@126.com>
2016-12-13 18:42:31 +0800

    Swift QuickSort

--- SortAlgorithm/SortAlgorithm-Swift/SortAlgorithm-Swift/main.swift
+++ SortAlgorithm/SortAlgorithm-Swift/SortAlgorithm-Swift/main.swift
@@ -33,9 +33,44 @@ bubbleSort(numbers: &numbers)
 print(numbers)
 
 
-func quickSort(numbers: inout [Int]) {
+print("")
+
+
+func quickSort1(_ list: [Int]) -> [Int] {
+    if list.count <= 1 { return list }
     
+    var low = [Int]()
+    var equal = [Int]()
+    var high = [Int]()
+    
+    let base = list[list.count / 2]
+    
+    for i in list {
+        if i < base {
+            low.append(i)
+        }
+        if (i == base) {
+            equal.append(i)
+        }
+        if (i > base) {
+            high.append(i)
+        }
+    }
+    return quickSort1(low) + quickSort1(equal) + quickSort1(high)
 }
+
+numbers = [1,3,10,6,4,5,9,2,8,7]
+print(numbers)
+print(quickSort1(numbers))
+
+
+print("")
+
+
+
+
+
+
 
 // 给一个整型数组和一个目标值，判断数组中是否有两个数字之和等于目标值
 func twoSum(nums: [Int], target: Int) -> Bool {
